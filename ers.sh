9cf21620832b4790bb4e2b8a1797140798c67ecd
John Laurino <john@phillyrd.org>
2015-11-12 19:58:45 +0000

    initial commit

---
+++ ers.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+# Local desired on / off hours:
+LON=9
+LOFFHOUR=16
+
+#Constants
+OFFMIN=30
+UTCOFFSET=5
+
+#Adjust for UTC
+ON=$(($LON + $UTCOFFSET))
+OFFH=$(($LOFFHOUR + $UTCOFFSET))
+ 
+SERVICE='omxplayer'
+
+while true; do
+
+H="$(date +%H)"
+M="$(date +%M)"
+
+if ((($H >= $ON)) && (($H < $OFFH))) || ((($H == $OFFH)) && (($M < $OFFMIN))) ; then  
+
+     if ps ax | grep -v grep | grep "$SERVICE" > /dev/null
+     then
+        echo "running" >> /dev/null
+     else
+        omxplayer /home/pi/engine_sounds.wav &
+     fi
+
+     #echo "time condition met. time is $H:$M, On is set to $ON, Off to $OFFH:$OFFMIN"
+
+     sleep 3s
+
+else 
+     if ps ax | grep -v grep | grep "$SERVICE" > /dev/null
+         then pkill "$SERVICE"
+
+     fi
+
+     #echo "time condition NOT met. time is $H:$M, On is set to $ON, Off to $OFFH:$OFFMIN" 
+
+     sleep 300s
+
+fi
+
+done
